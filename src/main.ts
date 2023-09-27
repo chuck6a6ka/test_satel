7dbe07369efffc6fdab75652893aea033bd92e7f
Andrey <oinkpage@gmail.com>
2023-09-27 08:25:57 +0300

    update cors policy

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: false});
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const swaggerConfig = new DocumentBuilder()
-      .setTitle('Документация для тестового задания от компании САТЕЛ')
-      .setVersion('1.0')
-      .addTag('files')
-      .build();
+    .setTitle('Документация для тестового задания от компании САТЕЛ')
+    .setVersion('1.0')
+    .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
+
 bootstrap();
